94a6b0947b9312edb1c935de561b3e8db2c12aa6
aguirrem22 <104278842+aguirrem22@users.noreply.github.com>
2025-12-11 22:31:44 +0100

    cr8 css

--- script/fondosIMG.js
+++ script/fondosIMG.js
@@ -1,16 +1,16 @@
 const bg = document.getElementById("backgroundIMG");
 
 const photos = [
-    "/img/img1.jpg",
-    "/img/img2.jpg",
-    "/img/img3.jpg",
-    "/img/img4.jpg",
-    "/img/img5.jpg",
-    "/img/img6.jpg",
-    "/img/img7.jpg",
-    "/img/img8.jpg",
-    "/img/img9.jpg",
-    "/img/img10.jpg",
+    "../img/img1.jpg",
+    "../img/img2.jpg",
+    "../img/img3.jpg",
+    "../img/img4.jpg",
+    "../img/img5.jpg",
+    "../img/img6.jpg",
+    "../img/img7.jpg",
+    "../img/img8.jpg",
+    "../img/img9.jpg",
+    "../img/img10.jpg",
 ];
 
 function changeBackground() {
